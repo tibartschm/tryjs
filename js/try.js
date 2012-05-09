2ed93168e03c2bbb32917a96a86ac1a2a8c6a834
Tim Bartsch <tibartschm@googlemail.com>
2012-05-09 09:03:47 +0200

    Code that has been entered since last error free run will be marked

--- js/try.js
+++ js/try.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
 
 	var session,
-		JSMode;
+		JSMode,
+		Range;
 	JSMode = require("ace/mode/javascript").Mode;
+	Range = require('ace/range').Range;
 	editor = ace.edit("editor");
 	session = editor.getSession();
 	
@@ -11,12 +13,24 @@ $(document).ready(function(){
 	session.on("change", function(evt){
 		var code,
 			lines,
-			i;
+			range,
+			startRange,
+			endRange,
+			i,
+			markers;
 		try{
 			code = editor.getSession().getValue();
 			eval(code);
+			markers = session.getMarkers();
+			for(i in markers){
+				session.removeMarker(i);
+			}
 		} catch(e) {
-			console.log(e.message);
+			startRange = evt.data.range.start;
+			endRange = evt.data.range.end;
+			var r = new Range(startRange.row, startRange.column,
+								endRange.row, endRange.column);
+			session.addMarker(r, "ace_selected_word","text");
 		}
 	});
 });
